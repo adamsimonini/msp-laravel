4fd7c1d81dde81d3ee81f4e8fdf7569ea93e0863
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-07-10 16:20:15 -0400

    user sessions working

--- public/js/app.js
+++ public/js/app.js
@@ -37173,7 +37173,7 @@ var staticRenderFns = [
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
               _vm._v(
-                "\n                    I'm an example component - DERKA DERKA\n                "
+                "\n                    Inshallah, brother! The fire rises! \n                "
               )
             ])
           ])
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component - DERKA DERKA
+                        Inshallah, brother! The fire rises! 
                     </div>
                 </div>
             </div>
