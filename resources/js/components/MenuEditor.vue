3d788be85024e80fc8aa14ba5df10a1af50aef1a
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-07-11 16:09:31 -0400

    added menu editing page
    admin only,
    new blade.php,
    artisan command used,
    vue routing component used

---
+++ resources/js/components/MenuEditor.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="">
+        <h1>Menu Editor</h1>
+        <!-- placeholder that swaps out the components based on the corresponding URL path -->
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import VueRouter from 'vue-router';
+    import CategroyManager from './CategoryManager.vue';
+    import MenuItem from './MenuItem.vue';
+
+    export default {
+        name: 'menu-editor',
+        router: new VueRouter({
+            routes: [
+                {
+                    path: '/categories',
+                    name: 'categories',
+                    component: CategroyManager
+                },
+                {
+                    path: '/',
+                    redirect: {name: 'categories'}
+                },
+                {
+                    path: '/add-item',
+                    name: 'add-item',
+                    component: MenuItem
+                },
+            ]
+        }),
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,18 @@ Auth::routes(['verify' => true]);
 
 Route::get('/', 'HomeController@index')->name('home')->middleware('verified');
 
+// can create routes by typing in route name, and the appropriate index.blade.php file for that route
 Route::get('/categories', 'CategoryController@index');
+
+Route::get('/akbar', 'CategoryController@index');
+
+Route::get('/menu-editor', 'AdminController@menu')->middleware('can:edit-menu');
+
+/* Chaper III episode I explains how to add new pages. It is as follows:
+    i) add a Route::get on web.php
+    ii) create a controller in artisan via "artisan make:controller {nameOfController}"
+    iii) edit newly created controller (for what reason, I don't fully understand)
+    iv) in "resources > views >" add the page as a .blade.php somewhere appropriately, and ensure it calls an appropriate Vue component
+    v) create the Vue component in "resources > js > components" 
+    vi) register the component globally in app.js, or else don't register it globally (and be sure to add .defaul to registration, or the component will fail to mount)
+*/
